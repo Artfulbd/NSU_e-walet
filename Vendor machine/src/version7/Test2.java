7716a680a45f1593710b178e3e6b64a0444d3f77
Artful <ariful.haque1@northsouth.edu>
2019-12-09 13:05:24 +0600

    Version7 1. Security update 2. Request URL now centralized on Master. 3. Warning message changed. 4. Update on validation viewer panel.

---
+++ Vendor machine/src/version7/Test2.java
@@ -0,0 +1,30 @@
+package version7;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Test2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+
+		final String regex = "[^*]+";
+		final String string = " Time 23:54  Date:20.11.19\n"
+			 + "*****************************\n"
+			 + " Item  (qty X rate) = price\n"
+			 + "-----------------------------\n"
+			 + " Tea          1X10  =  10\n"
+			 + " Coffee       1X25  =  25\n"
+			 + " Set Menu     2X145 =  290\n"
+			 + "------------------------------\n"
+			 + " Total payable amount:325";
+
+		final Pattern pattern = Pattern.compile(regex);
+		final Matcher matcher = pattern.matcher(string);
+		String res = "";
+		while (matcher.find()) {res+=(matcher.group(0).toString());}
+		System.out.println(res);
+
+	}
+
+}
